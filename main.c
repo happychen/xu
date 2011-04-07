c7e1a50a7a3a9a3b4e707651a87afafde43afde3
happychen <chenyifei_417@sina.com>
2011-04-07 19:15:27 +0800

    shiliujinzhi

--- main.c
+++ main.c
@@ -2,6 +2,23 @@
 
 int main(int argc, const char *argv[])
 {
+    int j = 0;
+    int i = 0;
+    int d = 0;
+    unsigned int mask = 0x0f; 
+    char str_h[20] = "0123456789abcdef";
     
+    puts("Please input a number :\n");
+    scanf("%d", &d);
+    
+    puts("The result :");
+    
+    j = 28;
+    for(i = 0;i < 8; i++,j -= 4) 
+    
+    {
+        printf("%c",str_h[(d >> j)&mask]);
+    }
+    printf("\n");
     return 0;
 }
