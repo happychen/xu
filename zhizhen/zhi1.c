d7b5f14864b6984a7408594addbfb1a9cdfd33e4
chen <chenyifei_417@sina.com>
2011-04-09 18:59:19 +0800

    use two str!

---
+++ zhizhen/zhi1.c
@@ -0,0 +1,26 @@
+//用两个指针消去空格
+
+#include <stdio.h>
+#include <string.h>
+
+#define N 100
+int main(int argc, const char *argv[])
+{
+    char str[N] = "   This is a C world!";
+    char *p = str;
+    char *s = str;
+
+    while(*s != '\0')
+    {
+        while(*s == ' ')s++;
+        if(p != s)
+        {
+            *p = *s;
+        }
+        p++;
+        s++;
+    }
+    *p = '\0';
+    printf("%d\t%s\n",strlen(str),str);
+    return 0;
+}
