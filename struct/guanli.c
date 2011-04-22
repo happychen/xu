f275d1fc20526657a82cd4f14a78e31d2e7ee7e7
chen <chenyifei_417@sina.com>
2011-04-22 18:23:53 +0800

    guanli save_link!

--- struct/guanli.c
+++ struct/guanli.c
@@ -31,12 +31,66 @@ void save_link(V_NODE *p)
         perror("malloc");
         exit(0);
     }
-    fprintf(fp, "%d\t%s", p->number, p->name);
+
+    while(p != NULL)
+    {
+
+        fprintf(fp, "%d\t%s\n", p->number, p->name);
+        p = p->next;     
+    }
     fclose(fp);
 }
+
+/*
+V_NODE *load_link(void)
+{
+    FILE *fp;
+    int num;
+    char na[20];
+    V_NODE *head;
+    V_NODE *p;
+
+    fp = fopen("text","r+");
+
+    if(fp == NULL)
+    {
+        perror("malloc");
+        exit(0);
+    }
+    if(fscanf(fp, "%d%s",&num, na) != EOF)
+    {
+        p = malloc(sizeof(V_NODE));
+        if (p == NULL) 
+        {
+            perror("malloc");
+            exit(0);
+        }
+        p->number = num;
+        strcpy(p->name, na);
+        p->next = NULL;
+        head = p;
+    }
+    while(fscanf(fp, "%d%s", &num, na) != EOF)
+    {
+        p->next = malloc(sizeof(V_NODE));
+        if (p->next == NULL) 
+        {
+            perror("malloc");
+            exit(0);
+        }
+        p->next->number = num;
+        strcpy(p->next->name, na);
+        p->next->next = NULL;
+        p = p->next;
+    }
+    return head;
+}
+*/
+
 int main(int argc, const char *argv[])
 {
     V_NODE *head = NULL;
+//    head =load_link();
 
     char flag = 0;
     while(!flag)
